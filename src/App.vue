3b8ce73958a64f58e3d0b8f237613b621c63c803
肖金 <xiaojin1992@126.com>
2018-07-31 20:02:39 +0800

    fuxian

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,83 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <md-field>
+      <md-field-item
+        name="name"
+        title="普通模式"
+        arrow="arrow-right"
+        align="right"
+        :value="selectorValue"
+        @click.native="showSelector">
+      </md-field-item>
+    </md-field>
+    <md-selector
+      v-model="isSelectorShow"
+      :data="data[0]"
+      :default-index="1"
+      :invalid-index="2"
+      title="普通模式"
+      @choose="onSelectorChoose($event)">
+    </md-selector>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { Selector, Field, FieldItem } from 'mand-mobile';
 
-@Component
-export default class App extends Vue {}
+@Component({
+  components: {
+    [Selector.name]: Selector,
+    [Field.name]: Field,
+    [FieldItem.name]: FieldItem,
+  },
+})
+export default class App extends Vue {
+  public isSelectorShow = false;
+  public data = [
+    [
+      {
+        text: '选项一',
+      },
+      {
+        text: '选项二',
+      },
+      {
+        text: '选项三',
+      },
+      {
+        text: '选项四',
+      },
+      {
+        text: '选项五',
+      },
+      {
+        text: '选项六',
+      },
+      {
+        text: '选项七',
+      },
+      {
+        text: '选项八',
+      },
+      {
+        text: '选项九',
+      },
+      {
+        text: '选项十',
+      },
+    ],
+  ];
+  public selectorValue = '选项二';
+
+  public showSelector() {
+    this.isSelectorShow = true;
+  }
+  public onSelectorChoose({text}: any) {
+      this.selectorValue = text;
+  }
+}
 </script>
 
 <style>
